d5f1e9e63a4e30ab455c897d6a7f29b335ebf842
Plucile <lucileperan@gmail.com>
2013-03-01 16:58:07 +0100

    Remove Acme Bundle

--- src/LPDW/TrainspovingBundle/Controller/APIController.php
+++ src/LPDW/TrainspovingBundle/Controller/APIController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace LPDW\TrainspovingBundle;
+namespace LPDW\TrainspovingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
- * @Route('/api')
+ * @Route("/api")
  */
 class APIController extends Controller
 {
