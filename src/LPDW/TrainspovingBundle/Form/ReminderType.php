a0c8641597376ce11f70c6acb36d31fc36f181be
Plucile <lucileperan@gmail.com>
2013-03-01 17:00:52 +0100

    Create Reminder controller CRUD

---
+++ src/LPDW/TrainspovingBundle/Form/ReminderType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LPDW\TrainspovingBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ReminderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('arrivalDate')
+            ->add('leeway')
+            ->add('rfid')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LPDW\TrainspovingBundle\Entity\Reminder'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'lpdw_trainspovingbundle_remindertype';
+    }
+}
