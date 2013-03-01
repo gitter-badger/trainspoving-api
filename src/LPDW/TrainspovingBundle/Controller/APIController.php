c44f055a2bf4d88a79805b9bd6219f855ad40176
Mathieu Darse <mattdarse@gmail.com>
2013-03-01 16:32:53 +0100

    Fix controller namespace

--- src/LPDW/TrainspovingBundle/Controller/APIController.php
+++ src/LPDW/TrainspovingBundle/Controller/APIController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LPDW\TrainspovingBundle;
+namespace LPDW\TrainspovingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
