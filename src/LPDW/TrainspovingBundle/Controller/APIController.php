0036407df752cddf9d698cca26d4c5e9968dca7b
Mathieu Darse <mattdarse@gmail.com>
2013-03-01 15:10:24 +0100

    Create API controller

---
+++ src/LPDW/TrainspovingBundle/Controller/APIController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LPDW\TrainspovingBundle;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route('/api')
+ */
+class APIController extends Controller
+{
+    
+}
