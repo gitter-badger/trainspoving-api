49e303f490811600a6b4d700bef9812c3e03515b
Mathieu Darse <mattdarse@gmail.com>
2013-03-01 15:10:12 +0100

    Create Reminder entity

---
+++ src/LPDW/TrainspovingBundle/Entity/Reminder.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace LPDW\TrainspovingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Reminder
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $arrivalDate;
+
+    /**
+     * Alert offset in seconds (i.e. taking in account travel time)
+     * 
+     * @ORM\Column(type="integer")
+     */
+    private $leeway;
+
+    /**
+     * @ORM\OneToOne(targetEntity="RFID")
+     * @ORM\JoinColumn(name="rfid_id", referencedColumnName="id")
+     */
+    private $rfid;
+
+    public function __construct()
+    {
+        $this->leeway = 0;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set arrivalDate
+     *
+     * @param \DateTime $arrivalDate
+     * @return Reminder
+     */
+    public function setArrivalDate($arrivalDate)
+    {
+        $this->arrivalDate = $arrivalDate;
+    
+        return $this;
+    }
+
+    /**
+     * Get arrivalDate
+     *
+     * @return \DateTime 
+     */
+    public function getArrivalDate()
+    {
+        return $this->arrivalDate;
+    }
+
+    /**
+     * Set leeway
+     *
+     * @param integer $leeway
+     * @return Reminder
+     */
+    public function setLeeway($leeway)
+    {
+        $this->leeway = $leeway;
+    
+        return $this;
+    }
+
+    /**
+     * Get leeway
+     *
+     * @return integer 
+     */
+    public function getLeeway()
+    {
+        return $this->leeway;
+    }
+
+    /**
+     * Get computed reminder date
+     */
+    public function getDate()
+    {
+        if (null === ($arrival = $this->getArrivalDate())) {
+            return null;
+        }
+        $date = clone $arrival;
+        $date->modify(sprintf('-%i seconds', $this->getLeeway()));
+
+        return $date;
+    }
+}
